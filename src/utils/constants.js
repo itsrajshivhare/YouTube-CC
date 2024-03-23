a7618caefb3785e301d6c7dee90b14809f6af9fe
Raj Aryaman <Rajaryaman2001@gmail.com>
2024-03-23 21:15:26 +0530

    Minor

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,6 +6,6 @@ export const YOUTUBE_VIDEOS_API =
   MY_GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const OFFSET_LIVE_CHAT = 30;
